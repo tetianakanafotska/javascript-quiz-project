ec495a15515f1e6353a029f9acdf2e4913ec75bf
tanyakanf <tanyakanf@gmail.com>
2024-04-03 15:42:57 +0200

    start Question class

--- src/question.js
+++ src/question.js
@@ -1,7 +1,10 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  shuffleChoices() {}
 }
